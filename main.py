71fdff9ebe8765e58d82350ae100da3c028a9506
daniel924 <ladenheim@gmail.com>
2016-10-25 21:35:09 -0400

    Appengine scaffolding

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
